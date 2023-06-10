7f1bd27a970bb55ff050410e3220e4f5002961d1
Amit Yadav <amy29981@gmail.com>
2023-06-10 22:47:27 +0200

    update

--- src/components/keplr/CosmosBalanceForm.js
+++ src/components/keplr/CosmosBalanceForm.js
@@ -37,41 +37,7 @@ function CosmosBalanceForm() {
 
     return (
         <Card className="text-center row">
-            <Card.Header>
-                <strong>Cosmos SDK Balance</strong>
-            </Card.Header>
             <Card.Body>
-                <Form className="col col-6 mx-auto" >
-
-                    <Form.Group className="mb-3" >
-                        <Form.Label>Select chain:</Form.Label>
-                        <Form.Select onChange={(e) => {handleSelect(e)}} >
-                            <option value={"cosmoshub-4"}>Cosmos Hub v4</option>
-                            <option value={"osmosis-1"}>Osmosis v1</option>
-                            <option value={"other"}>Other</option>
-                        </Form.Select>
-                    </Form.Group>
-
-                    <Form.Group className="mb-3" >
-                        <Form.Label>RPC endpoint:</Form.Label>
-                        <Form.Control type="text" placeholder="" value={rpcEndpoint} onChange={(e) => setRpcEndpoint(e.target.value)}  />
-                    </Form.Group>
-
-                    <Form.Group className="mb-3" >
-                        <Form.Label>Token:</Form.Label>
-                        <Form.Control type="text" placeholder="" value={token} onChange={(e) => setToken(e.target.value)}  />
-                    </Form.Group>
-
-                    <Form.Group className="mb-3" >
-                        <Form.Label>Token name:</Form.Label>
-                        <Form.Control type="text" placeholder="" value={tokenName} onChange={(e) => setTokenName(e.target.value)}  />
-                    </Form.Group>
-
-                    <Form.Group className="mb-3" >
-                        <Form.Label>Exponent:</Form.Label>
-                        <Form.Control type="number" placeholder="" value={exponent} onChange={(e) => setExponent(e.target.value)}  />
-                    </Form.Group>
-
                     <CosmosBalanceCard 
                         ref={balanceRef} 
                         chainId={chainId}
@@ -80,8 +46,6 @@ function CosmosBalanceForm() {
                         exponent={exponent} 
                         tokenName={tokenName} 
                         />
-
-                </Form>
             </Card.Body>
         </Card>
     )
